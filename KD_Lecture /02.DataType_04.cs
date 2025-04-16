3213f62cbfbf114f3b8c41f9a1dcc60d18e4e1e9
Kiho Park <152009026+KihoPark811@users.noreply.github.com>
2025-04-16 21:12:38 +0900

    Create 02.DataType_04.cs

---
+++ KD_Lecture /02.DataType_04.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+  int a = 100;
+  string b;
+
+  // Use this for initialization
+  void Start() {
+    // 정수를 문자열로 변환하는 방법
+    b = a.ToString(); 
+    b = "100";
+  }
+
+  // Update is called once per frame
+  void Update () {
+
+  }
