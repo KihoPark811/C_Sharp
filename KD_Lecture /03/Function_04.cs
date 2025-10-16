cd767b1bbac7e8c75648e43c49d4313d4e7d3ee3
Kiho Park <152009026+KihoPark811@users.noreply.github.com>
2025-10-16 18:13:31 +0900

    Create Function_04.cs

---
+++ KD_Lecture /03/Function_04.cs
@@ -0,0 +1,24 @@
+using System.Collection;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+  int intValue;
+
+  float floatValue = 10.5f;
+  float floatValue2 = 20.5f;
+
+  int FloatToInt(float _parameter, float _parameter2)
+  {
+      return (int)(_parameter + _parameter2); 
+  }
+
+  // Use this for initialization
+  void Start () {
+      print(FloatToInt(floatValue, floatValue2));
+  }
+
+  // Update is called once per frame
+  void Update () {
+
+  }
