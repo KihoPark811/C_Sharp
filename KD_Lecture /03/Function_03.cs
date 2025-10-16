578d15fa97637b1b3e86578c78bee8ffd838c0bb
Kiho Park <152009026+KihoPark811@users.noreply.github.com>
2025-10-16 18:09:24 +0900

    Update Function_03.cs

--- KD_Lecture /03/Function_03.cs
+++ KD_Lecture /03/Function_03.cs
@@ -8,16 +8,16 @@ public class Test : MonoBehaviour {
   float floatValue = 10.5f;
   float floatValue2 = 20.5f;
 
-  void FloatToInt(float _parameter, float _parameter2, string _stringParameter)
+  void FloatToInt(float _parameter, float _parameter2, string _stringParm = "디폴트값") // 디폴트값은 입력 파라미터가 없다면 디폴트값이 사용된다.
   {
-      intValue = (int)floatValue;
+      intValue = (int)(_parameter + _parameter2); 
       print(intValue);
+      print(_stringParm);
   }
 
   // Use this for initialization
   void Start () {
-      FloatToInt(floatValue);
-      FloatToInt(floatValue2);
+      FloatToInt(floatValue, floatValue2, "테스트");
   }
 
   // Update is called once per frame
